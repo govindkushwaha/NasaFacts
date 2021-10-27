e3358e19868c8eae3eb786fe3dc907cb1e3545cd
govindkushwaha <53311992+govindkushwaha@users.noreply.github.com>
2021-10-28 00:44:13 +0530

    first
    firsts

---
+++ src/Grid/Grid.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import GridItem from './GridItem';
+import styles from './Grid.module.css';
+import cx from 'classnames'
+import mediaStyles from './Media.module.css';
+export default class App extends React.Component{
+    updateClicked(id){
+        this.props.updateClicked(id);
+        
+    }
+
+    renderFacts(){
+        return this.props.facts.map(item => 
+            <GridItem  
+            key={item.id}
+            fact = {item}
+            updateClicked={this.updateClicked.bind(this)}
+            />
+        );
+    }
+
+    render(){
+        return(
+            <section className={cx(styles.grid,mediaStyles.grid)}>
+                {this.renderFacts()}
+            </section>
+        )
+    }
+}
